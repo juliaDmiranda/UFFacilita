48fd788af3be5eedac157723f8c4090c89143476
Júlia Miranda Rodrigues <60434126+juliaDmiranda@users.noreply.github.com>
2022-10-14 21:41:19 -0300

    add arquivos

---
+++ src/model/SemanaAcademica.java
@@ -0,0 +1,12 @@
+package model;
+import java.time.LocalDate;
+import java.util.List;
+
+public class SemanaAcademica extends Periodo{
+    private List<Evento> events;
+    
+    public SemanaAcademica(String _name, String _description, LocalDate _initialDate, LocalDate _finalDate){
+        super(_name, _description, _initialDate, _finalDate);
+        events  = new List<Evento>();
+    }
+}
