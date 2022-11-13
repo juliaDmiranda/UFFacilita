47de385dc463f2ed2576911214ea1130efadba82
Júlia Miranda Rodrigues <juliamrz2@outlook.com>
2022-11-13 20:23:32 -0300

    Adiciona leitura de arquivo json referente a Agenda

--- src/model/Utils/UFFacilitaConsts.java
+++ src/model/Utils/UFFacilitaConsts.java
@@ -2,6 +2,7 @@ package model.Utils;
 
 public enum UFFacilitaConsts {
     GRADE_JSON_PATH("C:\\RepositoriosFaculdade\\UFFacilita\\src\\data\\grade.json"),
+    AGENDA_JSON_PATH("C:\\Users\\User\\Desktop\\UFFacilita\\src\\data\\agenda.json")
     ;
 
     private String value;
